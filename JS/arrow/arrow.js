812e40cf143f2e4327d61758e5c83a4cde9c95cc
xavigalan <xavier.galan.perez@gmail.com>
2024-09-26 12:37:38 +0200

    arrow.js hecho, los 3 ejercicios

---
+++ JS/arrow/arrow.js
@@ -0,0 +1,23 @@
+//  1
+
+// const saludar = (nombre, apellido) => {
+//     console.log("Hola " + nombre + " " + apellido);
+// };
+// saludar("Xavi", "Galán Pérez");
+
+// 2
+
+// function persona(nombre, apellido, estudiando) {
+//     console.log(nombre, apellido, estudiando);
+// }
+// persona("Xavi", "Galán Pérez", true);
+
+// 3 
+
+// const saludarII = function (...numeros) {
+//     numeros.forEach(numero => {
+//         console.log(numero)
+//     });
+// };
+
+// saludarII(1,2,3,4,5);
