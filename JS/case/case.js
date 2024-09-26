b367d2bee77a78c7716022c914319da88d7df85c
xavigalan <xavier.galan.perez@gmail.com>
2024-09-26 13:40:36 +0200

    case hecho

---
+++ JS/case/case.js
@@ -0,0 +1,76 @@
+1
+let numeroaño = 5;
+
+switch (numeroaño) {
+    case 1:
+        console.log("Enero");
+        break;
+    case 2:
+        console.log("Febrero");
+        break;
+    case 3:
+        console.log("Marzo");
+        break;
+    case 4:
+        console.log("Abril");
+        break;
+    case 5:
+        console.log("Mayo");
+        break;
+    case 6:
+        console.log("Junio");
+        break;
+    case 7:
+        console.log("Julio");
+        break;
+    case 8:
+        console.log("Agosto");
+        break;
+    case 9:
+        console.log("Septiembre");
+        break;
+    case 10:
+        console.log("Octubre");
+        break;
+    case 11:
+        console.log("Noviembre");
+        break;
+    case 12:
+        console.log("Diciembre");
+        break;
+}
+
+2
+
+
+digito = "5";
+numero = Number(digito);
+console.log(numero);
+console.log(typeof (numero));
+
+if (numero <= 5) {
+    switch (numero) {
+        case 1:
+            console.log("Numero 1");
+            break;
+        case 2:
+            console.log("Numero 2");
+            break;
+        case 3:
+            console.log("Numero 3");
+            break;
+        case 4:
+            console.log("Numero 4");
+            break;
+        case 5:
+            console.log("Numero 5");
+            break;
+        case 6:
+            console.log("Numero 2");
+            break;
+    }
+}
+else {
+    console.log("El numero supera el 5");
+}
+
