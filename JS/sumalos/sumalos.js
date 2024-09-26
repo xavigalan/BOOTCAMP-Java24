6126cd972cb196ffb95ebc04606e17f26b96473e
xavigalan <xavier.galan.perez@gmail.com>
2024-09-26 13:55:20 +0200

    actualizado sumalos

--- JS/sumalos/sumalos.js
+++ JS/sumalos/sumalos.js
@@ -1,30 +1,30 @@
-// 1
+  1
 
-// function sumar(num1, num2, num3) {
-//     return num1 + num2 + num3;
-// }
-// resultado = sumar(1, 25, 7);
-// console.log("El restultado es: ", resultado);
+  function sumar(num1, num2, num3) {
+      return num1 + num2 + num3;
+  }
+  resultado = sumar(1, 25, 7);
+  console.log("El restultado es: ", resultado);
 
-// 2
+  2
 
-// function nombre(nombre, apellido1, apellido2) {
-//     return nombre + apellido1 + apellido2;
-// }
+  function nombre(nombre, apellido1, apellido2) {
+      return nombre + apellido1 + apellido2;
+  }
 
-// nombrecompleto = nombre("Xavi ", "Galán ", "Pérez");
-// console.log(nombrecompleto);
+  nombrecompleto = nombre("Xavi ", "Galán ", "Pérez");
+  console.log(nombrecompleto);
 
 
-// 3 
+  3 
 
-// function mayor(num1, num2) {
-//     primervalor = num1 > num2;
-//     if (primervalor) {
-//         return num1;
-//     } else
-//         return num2;
-// }
+  function mayor(num1, num2) {
+      primervalor = num1 > num2;
+      if (primervalor) {
+          return num1;
+      } else
+          return num2;
+  }
 
-// numeromayor = mayor(51, 50);
-// console.log(numeromayor);
+  numeromayor = mayor(51, 50);
+  console.log(numeromayor);
