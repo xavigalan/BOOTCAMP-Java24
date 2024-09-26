479f2d0a526cf0020d619c9c2b32c7fe5dea8902
xavigalan <xavier.galan.perez@gmail.com>
2024-09-26 13:49:59 +0200

    Redondear hecho

---
+++ JS/random/random.js
@@ -0,0 +1,11 @@
+// Como tener 1 o 0 con lo siguiente:
+
+aleatorio = Math.round(Math.random(0, 1));
+console.log(aleatorio);
+
+console.log("---------------------------");
+
+// Como redondear el número PI con Math.round.
+
+pi = Math.PI;
+console.log(Math.round(pi));
