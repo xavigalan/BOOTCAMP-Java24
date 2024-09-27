7cc934ff196101987014acbe90276e015a0d092a
xavigalan <xavier.galan.perez@gmail.com>
2024-09-27 10:08:32 +0200

    Bucles con while y for completado

---
+++ JS/bucle/bucle.js
@@ -0,0 +1,15 @@
+// Bucle con FOR
+
+for (let i = 0; i < 10 ; i++) {
+    const element = i;
+    console.log("i love code with for");
+}
+
+console.log("------------------------------")
+// Bucle con WHILE
+
+love = 0;
+while (love < 10) {
+    lovel = love++;
+    console.log("i love code with while");
+}
