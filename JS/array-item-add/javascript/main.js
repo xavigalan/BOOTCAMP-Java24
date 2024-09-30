c1b4f6c8820202131c6e1ebac217c66db1a8b9cd
xavigalan <xavier.galan.perez@gmail.com>
2024-09-30 13:58:21 +0200

    Arrays añadir

--- JS/array-item-add/javascript/main.js
+++ JS/array-item-add/javascript/main.js
@@ -1,2 +1,45 @@
 "use strict";
+// Donde van los elementos.
+const elementos = document.getElementById("elementos");
+// La lista de los elementos
+const items = [];
+// Actualizar lista en tiempo real.
+function updateDisplay() {
+    elementos.innerText = `[${items.join(", ")}]`;
+}
+// Botones añadir
 const push = document.getElementById("push");
+function addelast() {
+    items.push("🎃");
+    updateDisplay();
+}
+push.addEventListener("click", addelast);
+const unshift = document.getElementById("unshift");
+function addearly() {
+    items.unshift("🎈");
+    updateDisplay();
+}
+unshift.addEventListener("click", addearly);
+const añadir = document.getElementById("añadir");
+const insert = document.getElementById("insert");
+function addnum() {
+    const value = añadir.value;
+    if (value) {
+        items.push(`${value} 🎄`);
+        updateDisplay();
+    }
+}
+unshift.addEventListener("click", addearly);
+//Botones eliminar
+const pop = document.getElementById("pop");
+;
+function popet() {
+    if (items.length > 0) {
+        items.pop();
+        updateDisplay();
+    }
+}
+pop.addEventListener("click", popet);
+const shift = document.getElementById("shift");
+const remove = document.getElementById("remove");
+const eliminar = document.getElementById("eliminar");
--- JS/array-item-add/typescript/main.ts
+++ JS/array-item-add/typescript/main.ts
@@ -1,1 +1,58 @@
-const push = document.getElementById("push");
+// Donde van los elementos.
+const elementos = document.getElementById("elementos") as HTMLElement;
+
+// La lista de los elementos
+const items: string[] = [];
+
+
+// Actualizar lista en tiempo real.
+function updateDisplay() {
+    elementos.innerText = `[${items.join(", ")}]`;
+}
+
+// Botones añadir
+const push = document.getElementById("push") as HTMLButtonElement;
+function addelast() {
+    items.push("🎃");
+    updateDisplay();
+}
+push.addEventListener("click", addelast);
+
+const unshift = document.getElementById("unshift") as HTMLButtonElement;
+function addearly() {
+    items.unshift("🎈");
+    updateDisplay();
+}
+unshift.addEventListener("click", addearly);
+const añadir = document.getElementById("añadir") as HTMLInputElement;
+
+
+const insert = document.getElementById("insert");
+function addnum() {
+    const value = añadir.value;
+    if (value) {
+        items.push(`${value} 🎄`)
+        updateDisplay();
+    }
+}
+unshift.addEventListener("click", addearly);
+
+
+
+
+
+//Botones eliminar
+const pop = document.getElementById("pop") as HTMLButtonElement;;
+function popet() {
+    if (items.length > 0) {
+        items.pop();
+        updateDisplay();
+    }
+}
+pop.addEventListener("click", popet);
+
+
+const shift = document.getElementById("shift");
+const remove = document.getElementById("remove");
+const eliminar = document.getElementById("eliminar");
+
