b7871104262e3eca8b82761421520d27b9cd350c
xavigalan <xavier.galan.perez@gmail.com>
2024-10-01 13:57:42 +0200

    Inicio de piedra papel y tiejra humilde

---
+++ JS/piedraspapelesytijeras/piedra-humilde/javascript/main.js
@@ -0,0 +1,6 @@
+"use strict";
+const cartas = ["🥌", "🧻", "✂"];
+let mispuntos = 0;
+let puntoscomputer = 0;
+document.write("<h1>Bienvenido a Piedra 🥌 Papel 🧻 o Tijera ✂!</h1>");
+document.write("<p>Fichas:</p>" + (cartas.join(", ")));
