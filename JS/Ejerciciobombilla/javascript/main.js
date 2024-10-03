f56e5f74f1ecf66c6b51581b5b649f8ae0b4f91f
xavigalan <xavier.galan.perez@gmail.com>
2024-10-03 09:45:44 +0200

    Ejercicio Bombilla fix y completado 100%

--- JS/Ejerciciobombilla/javascript/main.js
+++ JS/Ejerciciobombilla/javascript/main.js
@@ -13,28 +13,38 @@ const brillo = document.getElementById("brillo");
 const brillonum = document.getElementById("brillonum");
 brillo.addEventListener('input', () => {
     brillonum.textContent = brillo.value;
-    bulb.style.opacity = brillo.value + "%";
+    bulb.style.opacity = (Number(brillo.value) / 100).toString();
+    if (brillo.value == "0" && bulb.src.endsWith("on.jpg")) {
+        bulb.src = "off.jpg";
+        interruptor.src = "boff.jpg";
+    }
 });
 brillonum.textContent = brillo.value;
-bulb.style.opacity = brillo.value + "%";
+bulb.style.opacity = (Number(brillo.value) / 100).toString();
 const rotate = document.getElementById("rotate");
 const rotatenum = document.getElementById("rotatenum");
 rotate.addEventListener('input', () => {
     rotatenum.textContent = rotate.value;
-    bulb.style.rotate = rotate.value + "deg";
+    bulb.style.transform = `rotate(${rotate.value}deg)`;
 });
 rotatenum.textContent = rotate.value;
-bulb.style.rotate = rotate.value + "deg";
+bulb.style.transform = `rotate(${rotate.value}deg)`;
 let encendido = false;
 function toggle() {
     encendido = !encendido;
     if (encendido) {
         interruptor.src = "bon.jpg";
         bulb.src = "on.jpg";
+        brillo.value = brillo.value = "100";
+        brillonum.textContent = brillo.value;
+        bulb.style.opacity = (Number(brillo.value) / 100).toString();
     }
     else {
         interruptor.src = "boff.jpg";
         bulb.src = "off.jpg";
+        brillo.value = brillo.value = "0";
+        brillonum.textContent = brillo.value;
+        bulb.style.opacity = (Number(brillo.value) / 100).toString();
     }
 }
 interruptor.addEventListener('click', toggle);
--- JS/Ejerciciobombilla/typescript/main.ts
+++ JS/Ejerciciobombilla/typescript/main.ts
@@ -10,37 +10,44 @@ zoom.addEventListener('input', () => {
 zoomnum.textContent = zoom.value;
 bulb.style.width = zoom.value + 'px';
 
-
 const brillo = document.getElementById("brillo") as HTMLInputElement;
 const brillonum = document.getElementById("brillonum") as HTMLDivElement;
 brillo.addEventListener('input', () => {
     brillonum.textContent = brillo.value;
-    bulb.style.opacity = brillo.value + "%";
+    bulb.style.opacity = (Number(brillo.value) / 100).toString();
+    if (brillo.value == "0" && bulb.src.endsWith("on.jpg")) {
+        bulb.src = "off.jpg";
+        interruptor.src = "boff.jpg";
+    }
 });
-brillonum.textContent = brillo.value;
-bulb.style.opacity = brillo.value + "%";
 
+brillonum.textContent = brillo.value;
+bulb.style.opacity = (Number(brillo.value) / 100).toString();
 
 const rotate = document.getElementById("rotate") as HTMLInputElement;
 const rotatenum = document.getElementById("rotatenum") as HTMLDivElement;
 rotate.addEventListener('input', () => {
     rotatenum.textContent = rotate.value;
-    bulb.style.rotate = rotate.value + "deg";
-})
+    bulb.style.transform = `rotate(${rotate.value}deg)`;
+});
 rotatenum.textContent = rotate.value;
-bulb.style.rotate = rotate.value + "deg";
+bulb.style.transform = `rotate(${rotate.value}deg)`;
 
 let encendido = false;
 function toggle() {
-    encendido = !encendido
+    encendido = !encendido;
     if (encendido) {
         interruptor.src = "bon.jpg";
         bulb.src = "on.jpg";
+        brillo.value = brillo.value = "100";
+        brillonum.textContent = brillo.value;
+        bulb.style.opacity = (Number(brillo.value) / 100).toString();
     } else {
         interruptor.src = "boff.jpg";
         bulb.src = "off.jpg";
+        brillo.value = brillo.value = "0";
+        brillonum.textContent = brillo.value;
+        bulb.style.opacity = (Number(brillo.value) / 100).toString();
     }
 }
-
 interruptor.addEventListener('click', toggle);
-
