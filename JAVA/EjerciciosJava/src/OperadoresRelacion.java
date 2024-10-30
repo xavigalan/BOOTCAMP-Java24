df91c15f5000bbed0ba78325a8a952fc7b7a3d84
xavigalan <xavier.galan.perez@gmail.com>
2024-10-30 10:56:19 +0100

    Operadores Relacion Finalizado

---
+++ JAVA/EjerciciosJava/src/OperadoresRelacion.java
@@ -0,0 +1,24 @@
+
+public class OperadoresRelacion {
+
+	public static void main(String[] args) {
+		int num1 = 1;
+		int num2 = 2;
+		int num3 = 3;
+		int num4 = 2;
+	System.out.println("Comprobar si num4 es igual a num2: " + "num4 es " + num4 + " num2 es " + num2 + " "+ (num1 == num2));
+	System.out.println("Comprobar si num2 es igual a num3: " + "num2 es " + num2 + " num3 es " + num3 + " "+ (num2 == num3));
+	System.out.println("Comprobar si num1 es distinto a num3: " + "num1 es " + num1 + " num3 es " + num3 + " "+ (num1 != num3));
+	System.out.println("Comprobar si num2 es distinto a num2: " + "num2 es " + num2 + " num2 es " + num2 + " "+ (num2 != num2));
+	System.out.println("Comprobar si num4 es menor a num3: " + "num4 es " + num4 + " num3 es " + num3 + " "+ (num4 < num3));
+	System.out.println("Comprobar si num3 es menor a num1: " + "num3 es " + num3 + " num1 es " + num1 + " "+ (num3 < num1));
+	System.out.println("Comprobar si num1 es mayor a num3: " + "num1 es " + num1 + " num3 es " + num3 + " "+ (num1 > num3));
+	System.out.println("Comprobar si num2 es mayor a num4: " + "num2 es " + num2 + " num4 es " + num4 + " "+ (num2 > num4));
+	System.out.println("Comprobar si num2 es menor o igual a num3: " + "num2 es " + num2 + " num3 es " + num3 + " "+ (num2 <= num3));
+	System.out.println("Comprobar si num2 es menor o igual a num4: " + "num2 es " + num2 + " num4 es " + num4 + " "+ (num2 <= num4));
+	System.out.println("Comprobar si num2 es menor o igual a num1: " + "num2 es " + num2 + " num1 es " + num1 + " "+ (num2 <= num1));
+	System.out.println("Comprobar si num1 es mayor o igual a num2: " + "num1 es " + num1 + " num2 es " + num2 + " "+ (num1 >= num2));
+
+	}
+
+}
