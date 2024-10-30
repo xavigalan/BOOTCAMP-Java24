2812a855ed8b10c87d98b8af8dd8e73d77a4032f
xavigalan <xavier.galan.perez@gmail.com>
2024-10-30 10:25:15 +0100

    Aritmeticos Incrementales Finalizado

---
+++ JAVA/EjerciciosJava/src/AritmeticosIncrementales.java
@@ -0,0 +1,21 @@
+
+public class AritmeticosIncrementales {
+
+	static int numero = 0;
+
+	static int decremento = 5;
+
+	public static void incrementar() {
+		System.out.println(++numero);
+	}
+	public static void decrementar() {
+		System.out.println(decremento--);
+	}
+	
+	public static void main(String[] args) {
+		incrementar();
+		decrementar();
+
+		decrementar();
+	}
+}
