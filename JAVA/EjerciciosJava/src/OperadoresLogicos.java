a6ff3afbe225024aa0b41ea3995ebf4fa30b49af
xavigalan <xavier.galan.perez@gmail.com>
2024-10-30 13:06:14 +0100

    Operadores Logicos

--- JAVA/EjerciciosJava/src/OperadoresLogicos.java
+++ JAVA/EjerciciosJava/src/OperadoresLogicos.java
@@ -2,7 +2,11 @@
 public class OperadoresLogicos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		boolean bool1 = true;
+		boolean bool2 = false;
+		boolean bool3 = false;
+		System.out.println("Comprobar si bool1 es igual al valor invertido de bool2 " + "bool1 = " + bool1 + " lo invertimos = " + !bool1 + " y después comparamos con " + "bool2 que es "+ bool2 + " y el resultado = " +(bool1 == bool2));
+		System.out.println("Comprobar si bool2 es igual al valor invertido de bool3 " + "bool2 = " + bool2 + " lo invertimos = " + !bool3 + " y después comparamos con " + "bool3 que es "+ bool3 + " y el resultado = " +(bool2 == bool3));
 
 	}
 
