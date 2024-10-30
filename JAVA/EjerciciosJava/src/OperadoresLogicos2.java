6116a57abb852217ac45cf5fea45d775b796a7f0
xavigalan <xavier.galan.perez@gmail.com>
2024-10-30 13:32:33 +0100

    estructuras condicionales

---
+++ JAVA/EjerciciosJava/src/OperadoresLogicos2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class OperadoresLogicos2 {
+
+    public static void main(String[] args) {
+    	Scanner scanner = new Scanner(System.in);
+    	System.out.println("Escribe un numero:");
+    	
+    	int numero = scanner.nextInt();
+    	
+    	String resultado = (numero % 2 == 0)? "par" : "impar";
+    	
+    	System.out.println("Tu número es " + numero + " y el resultado es "+ resultado +"." );
+    	
+    	int random = (int) (Math.random() * 2);  // Genera 0 o 1
+    	System.out.println("Tu número random es: " + random);
+    	String color = (random <= 0) ? "blanco" : "negro";
+
+    	System.out.println("El color es " + color + ".");
+
+    
+    
+    }
+}
