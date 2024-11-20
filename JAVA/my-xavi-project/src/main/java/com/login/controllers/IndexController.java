703dfbc364d93bd63a42a8f97b6a7fbdb55fd9d3
xavigalan <xavier.galan.perez@gmail.com>
2024-11-20 13:56:51 +0100

    Proyecto xavi

---
+++ JAVA/my-xavi-project/src/main/java/com/login/controllers/IndexController.java
@@ -0,0 +1,10 @@
+package com.login.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+	@RequestMapping("/hola") 
+		public String hola {
+	}
+}
